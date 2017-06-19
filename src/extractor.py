b1984bcdf2e4ee1fbcf934bb368d319fa41d1fd2
Carlos Cardoso Dias <ccd.leron@gmail.com>
2017-06-19 01:21:37 -0300

    Add unigram rule, pending memory optimizations

--- src/extractor.py
+++ src/extractor.py
@@ -6,17 +6,27 @@ from collections import defaultdict
 import pandas as pd
 from nltk import word_tokenize
 from nltk.corpus import stopwords
-
+from nltk import PorterStemmer
+import string
 
 # Preprocess text
 def preprocess_text (text, options):
-    # basic mandatory preprocess: remove html '<br />' and tokenize lower text
-    tokens = word_tokenize(text.lower().replace('<br />', ''))
+    # basic mandatory preprocess: remove html '<br />' and tokenize lower text removing punctuatuion
+    table = str.maketrans({ch: None for ch in string.punctuation})
+    tokens = [w.translate(table) for w in word_tokenize(text.lower().replace('<br />', ''))]
 
     # Remove stop words optionally
     if options.remove_stopwords:
         sw = stopwords.words('english')
         tokens = [w for w in tokens if w not in sw]
+
+    if options.remove_one_letter:
+        tokens = [w for w in tokens if len(w) > 1]
+
+    # Stem words
+    if options.stemmer:
+        stemmer = PorterStemmer()
+        tokens = [stemmer.stem(t) for t in tokens]
 
     return tokens
 
@@ -27,18 +37,28 @@ def preprocess_text (text, options):
 
 # This extractor only counts the presence of the words "good" and "bad"
 def dumb_extractor (tokens, dataset):
-    dataset['good'].append(tokens.count('good'))
-    dataset['bad'].append(tokens.count('bad'))
+    for t in tokens:
+        dataset['good'].append(t.count('good'))
+        dataset['bad'].append(t.count('bad'))
 
 # Do nothing yet
-def smart_extractor (tokens, dataset):
-    pass
+def unigram_extractor (tokens, dataset):
+    # create corpus
+    token_set = set()
+    for e in tokens:
+        token_set.update(e)
+    
+    # count elements for each row
+    for token in tokens:
+        for unit in token_set:
+            dataset[unit].append(token.count(unit))
 
 
 # This program receives the method extractor and the data directory as params
 def main():
     extract_modes = {
-        'dumb': dumb_extractor
+        'dumb': dumb_extractor,
+        'unigram': unigram_extractor
     }
 
     options = list(extract_modes.keys())
@@ -55,20 +75,33 @@ def main():
     parser.add_argument("-n", "--negative-label", default = "negative", help = "Label to be used in negative use cases")
 
     parser.add_argument("-r", "--remove-stopwords", action = 'store_true', help = "Remove common words in the englis vocabulary before processing")
+    parser.add_argument("-s", "--stemmer", action = 'store_true', help = "Use Porter Stemmer to stem words")
+    parser.add_argument("-o", "--remove-one-letter", action = 'store_true', help = "Remove one letter words")
 
     args = parser.parse_args()
+
+    # Text array
+    text = []
 
     # Read examples and parse features
     for type in ["pos", "neg"]:
         for file in glob("{}/{}/*.txt".format(args.data_directory, type)):
-            # Read file and preprocess into text variable
+            # Read file into text array
             with open(file) as f:
-                tokens = preprocess_text (f.read(), args)
+                text.append(f.read())
 
-            # Extract features with the given method
-            extract_modes[args.method] (tokens, dataset)
             # Put label
-            dataset['label'].append (args.positive_label if type == 'pos' else args.negative_label)
+            dataset['_result_label'].append (args.positive_label if type == 'pos' else args.negative_label)
+
+    # Extract features with the given method
+    tokens = []
+    for t in text:
+        tokens.append(preprocess_text (t, args))
+
+    extract_modes[args.method] (tokens, dataset)
+
+    for i in dataset.keys():
+        print("{} -> {}".format(i, len(dataset[i])))
 
     # Data set to data frame
     df = pd.DataFrame(dataset)
